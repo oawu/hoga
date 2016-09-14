42a8e4166ee36128eed9fa382324e5220b468b95
OA Wu <comdan66@gmail.com>
2016-09-14 19:02:28 +0800

    Fix kawashima look links.

--- application/cell/views/look_cell/footer/content.php
+++ application/cell/views/look_cell/footer/content.php
@@ -2,8 +2,8 @@
   <div>
     <div class='l'>Copyrights 2016  HOGA OPTICAL. All Rights Reserved.</div>
     <div class='r'>
-      <a href="">Facebook</a>
-      <a href="">Contact</a>
+      <a href="https://www.facebook.com/lookocchiali.tw/">Facebook</a>
+      <a href="mailto:info@hogaoptical.com.tw">Contact</a>
       <a href="<?php echo base_url ('look', 'stores');?>">Store</a>
       <a href="<?php echo base_url ('look#press');?>">Press</a>
       <a href="<?php echo base_url ('look#product');?>">Products</a>
